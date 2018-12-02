05c44f51cfc8280405a16a835c19d184548df33c
Ben Nelson <nelson.ben.c@gmail.com>
2018-12-02 15:06:18 -0700

    fix ruby silly nil.to_i = 0

--- extract_urls.rb
+++ extract_urls.rb
@@ -2,7 +2,7 @@
 
 num_files = 1
 
-line_count_cutoff = ARGV[0].to_i || 10_000
+line_count_cutoff = ARGV[0].nil? ? 10_000 : ARGV[0].to_i
 new_file = File.new("images/file_#{num_files}", 'w')
 
 # if original doesnt exist download original fall11_urls and unpack it
