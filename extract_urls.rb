869d2b2ddcd9398d105698d54bbd4048127ec1ff
Ben Nelson <nelson.ben.c@gmail.com>
2018-12-02 17:54:28 -0700

    add failsafe for number of files created

--- extract_urls.rb
+++ extract_urls.rb
@@ -3,6 +3,7 @@
 num_files = 1
 
 line_count_cutoff = ARGV[0].nil? ? 10_000 : ARGV[0].to_i
+num_files_cutoff = ARGV[1].nil? ? 1000 : ARGV[1].to_i
 new_file = File.new("images/file_#{num_files}", 'w')
 
 # if original doesnt exist download original fall11_urls and unpack it
@@ -15,6 +16,9 @@ File.open('fall11_urls.txt') do |f|
  line_count = 1
  f.each_line do |line|
   if line_count % line_count_cutoff == 0
+    # trying to not get excessive files, if you do 100 lines per file
+    # with fall 11 you wind up getting 100000 files and it starts having problems
+    exit(0) if num_files == num_files_cutoff
     num_files = num_files + 1
     new_file = File.new("images/file_#{num_files}", 'w')
   end
